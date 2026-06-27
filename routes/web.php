5deb8afa48504a3aeb7eef4979ed76cb4b2c0d30
Jose Armando <andiacespedesj@gmail.com>
2026-06-27 10:33:29 -0400

    Proyecto estudiante

--- routes/web.php
+++ routes/web.php
@@ -6,8 +6,13 @@ use App\Http\Controllers\StudentController;
 Route::get('/', function () { return redirect('/students'); });
 
 Route::get('/students', [StudentController::class, 'index']);
+
 Route::get('/students-list', [StudentController::class, 'getList']);
+
 Route::post('/students', [StudentController::class, 'store']);
+
 Route::get('/students/{student}', [StudentController::class, 'show']);
+
 Route::put('/students/{student}', [StudentController::class, 'update']);
+
 Route::delete('/students/{student}', [StudentController::class, 'destroy']);
