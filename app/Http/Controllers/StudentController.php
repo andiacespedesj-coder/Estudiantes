dd0a95e534771a62e094c94a0f83c32243a59719
Jose Armando <andiacespedesj@gmail.com>
2026-06-27 10:14:00 -0400

    Proyecto estudiante

--- app/Http/Controllers/StudentController.php
+++ app/Http/Controllers/StudentController.php
@@ -20,35 +20,26 @@ class StudentController extends Controller
         return response()->json(Student::all());
     }
 
-    /**
-     * Store a newly created resource in storage.
-     */
+    
     public function store(StoreStudentRequest $request)
     {
         $student = Student::create($request->validated());
         return response()->json($student, 201);
     }
 
-    /**
-     * Display the specified resource.
-     */
+
     public function show(Student $student)
     {
         return response()->json($student);
     }
 
-    /**
-     * Update the specified resource in storage.
-     */
+ 
     public function update(UpdateStudentRequest $request, Student $student)
     {
         $student->update($request->validated());
         return response()->json($student);
     }
 
-    /**
-     * Remove the specified resource from storage.
-     */
     public function destroy(Student $student)
     {
         $student->delete();
